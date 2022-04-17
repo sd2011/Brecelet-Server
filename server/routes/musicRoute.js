0ec15b75992e203365a740b7d1a2d816f2694657
sd2011 <stom96@gmail.com>
2022-04-17 14:11:05 +0300

    upload_music

---
+++ server/routes/musicRoute.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const express = require("express")
+const router = express.Router()
+const crypto = require("crypto")
+const mongoose = require("mongoose")
+const multer = require("multer")
+const {GridFsStorage} = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+
+const mongoPass = "nO30J8WXeQokhrec"
+const mongoURI = `mongodb+srv://stav:${mongoPass}@cluster0.uj18p.mongodb.net/music_brecelet`
+
+
+
+const conn = mongoose.createConnection(mongoURI)
+
+
+//init gfs
+conn.once('open', () =>{
+    const gfs = Grid(conn.db,mongoose.mongo);
+    gfs.collection('music')
+} )
+
+// Create storage engine
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'music'
+                };
+                resolve(fileInfo);
+            });
+        });
+    }
+});
+const upload = multer({ storage });
+
+
+
+
+
+router.route("/music").post( upload.single("sound"),(req,res) =>{
+    console.log(req.file)
+})
+
+module.exports = router
---
+++ server/server.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+const srcPath = path.join(__dirname, '..', '')
+const port = process.env.PORT || 3000
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static(srcPath));
+app.use(express.urlencoded());
+
+app.use("/", require("./routes/musicRoute"))
+
+app.listen(port, () => {
+    console.log(`Server is up! ${port}`);
+});
