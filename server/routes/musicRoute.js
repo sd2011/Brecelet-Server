9fe005b4b11a70192a7f00c687209544c3ab8153
sd2011 <stom96@gmail.com>
2022-04-30 20:49:37 +0300

    fileSize try

--- server/routes/musicRoute.js
+++ server/routes/musicRoute.js
@@ -14,10 +14,13 @@ const mongoURI = `mongodb+srv://stav:${mongoPass}@cluster0.uj18p.mongodb.net/mus
 
 const conn = mongoose.createConnection(process.env.MONGODB_URI || mongoURI)
 
-
+let gfs;
 //init gfs
 conn.once('open', () =>{
-    const gfs = Grid(conn.db,mongoose.mongo);
+    gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+        bucketName: 'music'
+    })
+    gfs = Grid(conn.db,mongoose.mongo);
     gfs.collection('music')
 } )
 
@@ -40,7 +43,7 @@ const storage = new GridFsStorage({
         });
     }
 });
-const upload = multer({ storage });
+const upload = multer({ storage, limits: {fieldSize: "50mb"} });
 
 
 
@@ -51,7 +54,34 @@ router.route("/music").post( upload.single("sound"),(req,res) =>{
 })
 
 router.route("/music").get( (req,res) =>{
-    res.send("hello")
+    gfs.files.find().sort({"uploadDate" :-1}).limit(50).toArray((err,files)=>{
+        if(!files || files.length===0){
+            return res.status(404).json({err:"no files"})
+        }
+        return res.json({files})
+    })
+
+router.route('/music/:filename').get((req, res) => {
+        gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+            // Check if file exist
+            if (!file || file.length === 0) {
+                return res.status(404).json({
+                    err: 'No file exists'
+                });
+            }
+
+            // Check if a music file
+            console.log(file.contentType)
+            if (file.contentType === 'audio/mpeg' || file.contentType === 'audio/wave') {
+                const readStream =  gridfsBucket.openDownloadStream(file._id);
+                readStream.pipe(res);
+            } else {
+                res.status(404).json({
+                    err: 'Not an image'
+                });
+            }
+        });
+    });
 })
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -3,14 +3,17 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 const srcPath = path.join(__dirname, '..', '')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
+const cors = require('cors')
 
+app.use(cors())
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
 
 app.use(express.static(srcPath));
 app.use(express.urlencoded());
